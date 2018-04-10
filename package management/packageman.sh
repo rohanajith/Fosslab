bde0791365ccac4b88eaeeb5c62b1a3c7deb738b
roha5090 <rohanajith5090@gmail.com>
2018-04-11 00:32:00 +0530

    package

---
+++ package management/packageman.sh
@@ -0,0 +1,8 @@
+sudo dpkg -i /home/roh/Downloads/Simplenote-linux-1.1.3.deb 
+sudo apt-get update
+sudo apt-get upgrade
+apt-cache search Simplenote
+dpkg -s Simplenote
+sudo apt-get install vim
+sudo apt-get remove vim
+
